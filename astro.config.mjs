078210e6b8e706e05938604475033d0c63a6982e
ambrosayos <ambrosayos@mail.ru>
2024-08-10 21:48:30 +0300

    fixed sharp-images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "middleware",
+  }),
+  experimental: {
+    serverIslands: true,
+  },
+});
